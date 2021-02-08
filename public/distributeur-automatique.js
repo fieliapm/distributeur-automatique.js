68c4e8c6d7d2a9bbab38b03c2de86ee9ecb1d260
fieliapm <fieliapm@gmail.com>
2021-02-08 23:17:19 +0800

    add page for vendor machine points evaluation

---
+++ public/distributeur-automatique.js
@@ -0,0 +1,152 @@
+/*
+ * distributeur-automatique.js - evaluate max purchase strategy
+ * Copyright (C) 2021-present Himawari Tachibana <fieliapm@gmail.com>
+ *
+ * This file is part of date-boundary.js
+ *
+ * distributeur-automatique.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* jshint esversion: 6 */
+
+
+function _init2DArray(m, n) {
+    var a = new Array(m);
+    for (var i = 0; i < a.length; ++i) {
+        a[i] = new Array(n).fill(0);
+    }
+    return a;
+}
+
+
+function MaxPurchaseSolutionCache(prices) {
+    this.prices = prices;
+    this.solutionMap = {};
+
+    this.findMaxPurchase = function (priceLen, budget) {
+        return this.findMaxPurchaseFast(priceLen, budget);
+    };
+
+    this.findMaxPurchaseFast = function (priceLen, budget) {
+        var solutionKey = [priceLen, budget].toString();
+        if (this.solutionMap.hasOwnProperty(solutionKey)) {
+            return this.solutionMap[solutionKey];
+        } else {
+            var solutionValue = this.findMaxPurchaseSlow(priceLen, budget);
+            this.solutionMap[solutionKey] = solutionValue;
+            return solutionValue;
+        }
+    };
+
+    this.findMaxPurchaseSlow = function (priceLen, budget) {
+        if (priceLen <= 0) {// || budget <= 0){
+            return 0;
+        }
+
+        var maxPurchase = this.findMaxPurchaseFast(priceLen - 1, budget);
+
+        var price = this.prices[priceLen - 1];
+        if (budget >= price) {
+            var purchase = price + this.findMaxPurchaseFast(priceLen, budget - price);
+            maxPurchase = Math.max(maxPurchase, purchase);
+        }
+
+        return maxPurchase;
+    };
+}
+
+
+function MaxPurchaseSolutionDP(prices, budget) {
+    function buildMaxPurchaseSolution(prices, budget) {
+        var dp = _init2DArray(prices.length + 1, budget + 1);
+
+        for (var priceLen = 1; priceLen <= prices.length; ++priceLen) {
+            dp[priceLen][0] = 0;
+            for (var partOfBudget = 1; partOfBudget <= budget; ++partOfBudget) {
+                var maxPurchase = dp[priceLen - 1][partOfBudget];
+
+                var price = prices[priceLen - 1];
+                if (partOfBudget >= price) {
+                    var purchase = price + dp[priceLen][partOfBudget - price];
+                    maxPurchase = Math.max(maxPurchase, purchase);
+                }
+                dp[priceLen][partOfBudget] = maxPurchase;
+            }
+        }
+
+        return dp;
+    }
+
+    this.solutionTable = buildMaxPurchaseSolution(prices, budget);
+
+    this.findMaxPurchase = function (priceLen, budget) {
+        return this.solutionTable[priceLen][budget];
+    };
+}
+
+
+function evaluateMaxPurchase(prices, budget) {
+    var solutions = [];
+    var path = [];
+    function traceMaxPurchase(maxPurchaseSolution, prices, priceLen, budget) {
+        if (priceLen <= 0) {// || budget <= 0){
+            solutions.push(path.slice());
+            return 0;
+        }
+
+        var maxPurchase = maxPurchaseSolution.findMaxPurchase(priceLen, budget);
+
+        var purchaseWithoutThisItem = maxPurchaseSolution.findMaxPurchase(priceLen - 1, budget);
+        if (maxPurchase === purchaseWithoutThisItem) {
+            traceMaxPurchase(maxPurchaseSolution, prices, priceLen - 1, budget);
+        }
+
+        var price = prices[priceLen - 1];
+        if (budget >= price) {
+            var purchaseWithThisItem = price + maxPurchaseSolution.findMaxPurchase(priceLen, budget - price);
+            if (maxPurchase === purchaseWithThisItem) {
+                path.push(price);
+                traceMaxPurchase(maxPurchaseSolution, prices, priceLen, budget - price);
+                path.pop();
+            }
+        }
+
+        return maxPurchase;
+    }
+
+    //var maxPurchaseSolution = new MaxPurchaseSolutionCache(prices);
+    var maxPurchaseSolution = new MaxPurchaseSolutionDP(prices, budget);
+
+    var maxPurchase = traceMaxPurchase(maxPurchaseSolution, prices, prices.length, budget, []);
+
+    return {
+        maxPurchase: maxPurchase,
+        solutions: solutions,
+    }
+}
+
+
+var a = [24, 29, 37, 33, 22, 109, 38, 32, 57, 30, 132, 19, 14, 27, 28];
+var b = 141;
+//var b = 250;
+//var a = [12,13];
+//var b = 30;
+//var a = [2,3,4];
+//var b = 10;
+s = new Date();
+var r = evaluateMaxPurchase(a, b);
+e = new Date();
+console.log(e - s);
+//var r = FindExactPurchaseDP(a, b);
+console.log([r.maxPurchase, r.solutions.length])
